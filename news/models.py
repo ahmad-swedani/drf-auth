7a8c4b3f4de604cd8e2b585e24ce6e11b2c7132b
ahmad-swedani <swedani390@gmail.com>
2020-10-01 06:22:09 +0300

    done lab Authentication

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    image = models.URLField(default='https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/136893056/original/c077ae2c46497ef39ba6db4b67a2b066d202bc0b/make-restful-api-using-django-rest-framework.png')
+    short_content = models.TextField()
+    full_content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
